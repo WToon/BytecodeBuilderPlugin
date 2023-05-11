376a561edc76e2af3cd417f0a590c81664570bc7
Toon Willemot <toon.willemot@guardsquare.com>
2023-05-11 11:56:07 +0200

    Use JTextAre for the InputPanel

--- src/main/java/com/guardsquare/bytecodebuilder/BytecodeBuilderToolWindowFactory.java
+++ src/main/java/com/guardsquare/bytecodebuilder/BytecodeBuilderToolWindowFactory.java
@@ -9,9 +9,10 @@ import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Document;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class BytecodeBuilderToolWindowFactory
 implements   ToolWindowFactory
@@ -20,7 +21,7 @@ implements   ToolWindowFactory
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow)
     {
         Content content = ContentFactory.getInstance().createContent(
-                new BytecodeBuilderToolWindowContent().contentPanel, "BytecodeBuilder", false);
+                new BytecodeBuilderToolWindowContent().contentPanel, "", false);
         toolWindow.getContentManager().addContent(content);
     }
 
@@ -28,28 +29,44 @@ implements   ToolWindowFactory
     private static class BytecodeBuilderToolWindowContent
     {
         public JPanel       contentPanel    = new JPanel();
-        public JTextField   inputField      = new JTextField();
+        public JTextArea    inputField      = new JTextArea();
         public JTextArea    outputField     = new JTextArea();
-
-        public String inputSnippet;
 
         public BytecodeBuilderToolWindowContent()
         {
-            inputField.addActionListener(
-                    new ActionListener()
-                    {
-                        @Override
-                        public void actionPerformed(ActionEvent e)
-                        {
-                            inputSnippet = inputField.getText();
-                            outputField.setText(inputSnippet);
-                        }
-                    });
+            setupInputPanel();
 
             contentPanel.setLayout(new GridLayout(2,1));
             
             contentPanel.add(inputField);
             contentPanel.add(outputField);
+        }
+
+
+        /**
+         * Sets up the input panel.
+         */
+        private void setupInputPanel()
+        {
+            Document inputFieldDocument = inputField.getDocument();
+            inputFieldDocument.addDocumentListener(
+                    new DocumentListener()
+                    {
+                        @Override
+                        public void changedUpdate(DocumentEvent e) { /* Never triggered by JTextAre. */ }
+
+                        @Override
+                        public void insertUpdate(DocumentEvent e)
+                        {
+                            outputField.setText(inputField.getText());
+                        }
+
+                        @Override
+                        public void removeUpdate(DocumentEvent e)
+                        {
+                            outputField.setText(inputField.getText());
+                        }
+                    });
         }
     }
 }
